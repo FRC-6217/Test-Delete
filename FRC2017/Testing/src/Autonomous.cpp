255569f51a9107fdc4a66ccebf12fa3f3b462715
ewilden2017 <ewilden2017@gmail.com>
2017-02-07 18:28:28 -0600

    Added class file

---
+++ FRC2017/Testing/src/Autonomous.cpp
@@ -0,0 +1,18 @@
+/*
+ * Autonomous.cpp
+ *
+ *  Created on: Feb 7, 2017
+ *      Author: Evan
+ */
+
+#include <Autonomous.h>
+
+Autonomous::Autonomous() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Autonomous::~Autonomous() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ FRC2017/Testing/src/Autonomous.h
@@ -0,0 +1,17 @@
+/*
+ * Autonomous.h
+ *
+ *  Created on: Feb 7, 2017
+ *      Author: Evan
+ */
+
+#ifndef SRC_AUTONOMOUS_H_
+#define SRC_AUTONOMOUS_H_
+
+class Autonomous {
+public:
+	Autonomous();
+	virtual ~Autonomous();
+};
+
+#endif /* SRC_AUTONOMOUS_H_ */
